c18543393bc6b94030009365453ab496aaad3951
HuongNguyen <nguyenhuong150905@gmail.com>
2025-12-02 22:34:03 +0700

    change root

--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const url = "http://localhost:3000/api/"
-// const url = "https://vocab-builder-aa45.onrender.com/api/"
+// const url = "http://localhost:3000/api/"
+const url = "https://vocab-builder-aa45.onrender.com/api/"
 
 
 export default url
