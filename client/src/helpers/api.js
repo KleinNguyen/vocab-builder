0f1979b372b18699561c6c6938ea76d3003b094c
HuongNguyen <nguyenhuong150905@gmail.com>
2025-12-02 21:52:40 +0700

    fix api.js for render url

--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
-const url = "http://localhost:3000/api/"
+// const url = "http://localhost:3000/api/"
+const url = "https://vocab-builder-aa45.onrender.com/api/"
+
 
 export default url
